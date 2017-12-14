521e3d5738efe52d7dd7c6d04029568a2dcb4335
Perceval WAJSBURT <perceval.wajsburt@gmail.com>
2017-12-14 12:05:28 +0100

    changed network configs

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def main():
         pickle.dump(headlines, open(HEADLINES_PATH, "wb"))
 
     # Start the app
-    app.run()
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == "__main__":
     main()
